16f9b63f9404f6e6a5b2169b62dd3e6d0da68fbf
Gino <ginogallo74@yahoo.com>
2024-10-17 12:58:28 -0400

    Initial header build

---
+++ src/screens/header/HeaderScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeaderScreen = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <div>About</div>
+      <div>Projects</div>
+      <div>Resume</div>
+      <div>Contact</div>
+      <div>LI Logo</div>
+      <div>GH Logo</div>
+    </div>
+  );
+};
+
+export default HeaderScreen;
