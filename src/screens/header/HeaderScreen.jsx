ab292036bbdd9a8e58a973e225fea55e6ff18937
Gino <ginogallo74@yahoo.com>
2024-10-23 13:15:54 -0400

    implemented semantic navbar

--- src/screens/header/HeaderScreen.jsx
+++ src/screens/header/HeaderScreen.jsx
@@ -1,16 +1,63 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import navBg from "../../assets/nav-bg.png";
+import LinkedInLogo from "../../assets/linkedin-logo.svg";
+import githubLogo from "../../assets/github.svg";
+import "./HeaderScreen.css";
 
 const HeaderScreen = () => {
   return (
-    <div>
-      <div>Home</div>
-      <div>About</div>
-      <div>Projects</div>
-      <div>Resume</div>
-      <div>Contact</div>
-      <div>LI Logo</div>
-      <div>GH Logo</div>
-    </div>
+    <nav className="nav-wrapper">
+      <ul className="text-wrapper">
+        <div className="home-wrapper">
+          <li>
+            <NavLink to={"/home"} className="all-text home">
+              Home
+            </NavLink>
+          </li>
+        </div>
+        <div className="text-group">
+          <li>
+            <NavLink to={"/about"} className="all-text ">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/project"} className="all-text projects">
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/resume"} className="all-text ">
+              Resume
+            </NavLink>
+          </li>
+        </div>
+        <div className="contact-wrapper">
+          <li>
+            <NavLink to={"/contact"} className="all-text contact">
+              Contact
+            </NavLink>
+          </li>
+        </div>
+      </ul>
+      <ul className="social-wrapper">
+        <li className="linkedin">
+          <a href="https://www.linkedin.com" target="_blank" className="social">
+            <img src={LinkedInLogo} alt="" />
+          </a>
+        </li>
+        <li className="github">
+          <a
+            href="https://www.github.com"
+            target="_blank"
+            className="social github"
+          >
+            <img src={githubLogo} alt="" />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
